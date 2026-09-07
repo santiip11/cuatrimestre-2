ba3ab43abb0a243a4d0653b0eabf5bf7233bbab2
santiip <santiipar10@gmail.com>
2026-09-07 09:57:40 -0300

    ejercicio 37

---
+++ funciones_string.php
@@ -0,0 +1,35 @@
+<?php
+
+
+$cadena_texto="Hola mundo";
+
+echo strtolower($cadena_texto);
+
+//todo minuscula //
+
+echo "<br>";
+
+echo $cadena_texto=strtolower($cadena_texto);
+
+// todo minuscula alternativo //
+
+echo "<br>";
+
+echo strtoupper($cadena_texto);
+
+// todo mayus //
+
+echo "<br>";
+
+$cadena_texto=ucfirst($cadena_texto);
+
+// pone arriba de todo el texto elegido //
+
+echo "<br>";
+
+$cadena_texto=ucwords($cadena_texto);
+
+echo $cadena_texto;
+
+// pone todas las primeras letras de las palabras en mayuscula"//
+
---
+++ funciones_string2.php
@@ -0,0 +1,11 @@
+<?php
+
+$cadena_texto="hola mundo";
+
+$longitud=strlen($cadena_texto);
+
+echo $cadena_texto." tiene ".$longitud." caracteres. "."<br>";
+
+$palabras=str_word_count($cadena_texto);
+
+echo $cadena_texto." tiene ".$palabras ." palabras. "."<br>";
